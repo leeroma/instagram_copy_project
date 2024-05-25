c028fafbcb1daccffafd0415a8f66ed19e60bc65
Roma <roma.li0071@gmail.com>
2024-05-25 15:57:41 +0500

    added account model, authentication, login page, made migrations

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from accounts.managers import AccountManager
+
+
+class Account(AbstractUser):
+    SEX_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+    )
+
+    avatar = models.ImageField('Аватар', upload_to='avatars', blank=False, null=False)
+    bio = models.TextField('Информация о пользователе', max_length=3000, blank=True, null=True)
+    phone_number = models.IntegerField('Номер телефона', blank=True, null=True)
+    sex = models.CharField('Пол', choices=SEX_CHOICES, max_length=6, blank=True, null=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'avatar', 'password']
+
+    objects = AccountManager()
+
+    class Meta:
+        verbose_name = 'Account'
+        verbose_name_plural = 'Accounts'
+
+    def __str__(self):
+        return self.username
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class LoginView(TemplateView):
+    template_name = 'accounts/login.html'
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('index')
+
+        context = {'message': 'Invalid username or password'}
+
+        return render(request, self.template_name, context=context)
